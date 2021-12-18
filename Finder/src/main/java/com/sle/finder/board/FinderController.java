9ff0b40ecfc9255fb36dee8d65b8eae9754acab1
wanilly <wanimagic@gmail.com>
2021-12-18 10:38:29 +0900

    css commit

--- Finder/src/main/java/com/sle/finder/board/FinderController.java
+++ Finder/src/main/java/com/sle/finder/board/FinderController.java
@@ -1,11 +1,22 @@
 package com.sle.finder.board;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Controller 
 @RequestMapping(value="/board")
@@ -39,6 +50,7 @@ public class FinderController {
 		model.addAttribute("finderVO", finderVO);
 		return "editform";
 	}
+
 	
 	@RequestMapping(value = "/editok", method = RequestMethod.POST)
 	public String editPostOk(FinderVO vo) {
@@ -60,5 +72,41 @@ public class FinderController {
 			System.out.println("삭제 되었습니다.");
 		
 		return "redirect:../list";
+	}
+	
+	@RequestMapping(value = "/post", method = RequestMethod.GET)
+	public String home(Model model) {
+		return "post";
+	}
+	
+	@RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
+	public String fileUpload(MultipartFile upload, HttpServletRequest request) {
+		
+		String saveDir = request.getSession().getServletContext().getRealPath("/resources/img");
+		File dir = new File(saveDir);
+		
+        if(!dir.exists()) { //업로드 디렉토리가 존재하지 않으면 생성
+            dir.mkdirs();
+        }
+        
+        
+        	
+		// 기존 파일 이름을 받고 확장자 저장 
+		String orifileName = upload.getOriginalFilename(); 
+		String ext = orifileName.substring(orifileName.lastIndexOf(".")); 
+		
+		// 이름 값 변경을 위한 설정 
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmssSSS"); 
+		int rand = (int)(Math.random()*1000); 
+		// 파일 이름 변경 
+		String reName = sdf.format(System.currentTimeMillis()) + "_" + rand + ext; 
+		// 파일 저장 
+		try { 
+			upload.transferTo(new File(saveDir + "/" + reName)); 
+		}	catch (IOException e) {
+			e.printStackTrace(); 
+		} 
+    		 
+        return "list";
 	}
 }
---
+++ Finder/src/main/webapp/WEB-INF/views/post.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>파일 업로드 테스트</title>
+</head>
+<body>
+    <h1>파일 업로드</h1>
+    <br>
+    <form method="post" enctype="multipart/form-data" action="fileUpload">
+        <table>
+            <tr>
+                <td><label>파일</label></td>
+                <td><input type="file" name="photo"></td>
+            </tr>
+        </table>
+        <input type="submit" value="업로드" name="submit">
+    </form>
+</body>
+</html>
